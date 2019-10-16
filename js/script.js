016f316541771b0fe20bff6c3bc40dbfb6d97d82
Mikhail Razuvayev <Rock.is.Life.352@gmail.com>
2019-10-16 21:34:44 +0300

    16.10.19 - 21:34

--- js/script.js
+++ js/script.js
@@ -28,3 +28,27 @@ arrow[1].addEventListener('click', function() {
 arrow[2].addEventListener('click', function() {
 	clickFunction(arrow[2],text[2]);
 });
+
+
+/*Прокрутка блоков в разделе отзывов*/ 
+var buttonReviewPrev = document.querySelector('.review__arrow--prev');
+var buttonReviewNext = document.querySelector('.review__arrow--next');
+var reviewArray = document.querySelectorAll('.review__element');
+var a = 0, b;
+function reviewNext() {
+	if((reviewArray.length - a) > 1)
+	{	  			
+		reviewArray[a].style.order = a+1;
+		a++;
+	}
+};
+function reviewPrev() {
+ 	if(a>0)
+ 	{
+ 		b = a - 1;
+  		reviewArray[b].style.order = 0;
+  		a--;
+  	}
+ };
+buttonReviewNext.addEventListener('click', reviewNext);
+buttonReviewPrev.addEventListener('click', reviewPrev);
